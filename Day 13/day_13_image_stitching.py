b386e023bfa8f5ae2d046a9748d3a4f6663fd73b
Tony Luna <anluna1998@gmail.com>
2024-01-21 22:15:52 -0800

    Created day 13 folder

---
+++ Day 13/day_13_image_stitching.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jan 21 22:08:27 2024
+
+@author: anlun
+"""
+
+import cv2
+import os
+
+
+def main(images_directory):
+    print("Hello!")
+    
+if __name__ == "__main__":
+    images_directory = "Images"
+    
+    main(images_directory)
